098bafd3b49ee5c83bf4f610c8513e5e8864d763
Johan Cwiklinski <johan@x-tnd.be>
2011-06-03 15:01:59 +0000

    Add paypal plugin (WIP)

---
+++ lang/lang_english.php
@@ -0,0 +1,15 @@
+<?php
+// This file was automatically generated on Fri Jun  3 14:24:25 2011 (GMT)
+// Don't modify it by hand, rather use the target lang from the Makefile.
+
+
+// ../templates/default/paypal_form.tpl:66 
+$lang['Amount'] = 'Amount';
+
+// ../templates/default/paypal_form.tpl:37 
+$lang['Select an option below, then click \'Pay\' to proceed. Once your paiment validated, an entry will be automatically added in the contributions table and staff members will receive a notification mail.'] = 'Select an option below, then click \'Pay\' to proceed. Once your paiment validated, an entry will be automatically added in the contributions table and staff members will receive a notification mail.';
+
+// ../templates/default/paypal_form.tpl:70 
+$lang['Proceed'] = 'Proceed';
+
+?>
