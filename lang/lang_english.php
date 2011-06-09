4c73678f8483cc754b50cab27c110a4318d1f0ef
Johan Cwiklinski <johan@x-tnd.be>
2011-06-09 15:25:53 +0000

    i18n

--- lang/lang_english.php
+++ lang/lang_english.php
@@ -1,15 +1,51 @@
 <?php
-// This file was automatically generated on Fri Jun  3 14:24:25 2011 (GMT)
+// This file was automatically generated on Thu Jun  9 15:21:54 2011 (GMT)
 // Don't modify it by hand, rather use the target lang from the Makefile.
 
 
-// ../templates/default/paypal_form.tpl:66 
+// ../templates/default/paypal_form.tpl:56 
+$lang['WARNING: If you enter an amount below, make sure that it is not lower than the amount of the option you\'ve selected.'] = 'WARNING: If you enter an amount below, make sure that it is not lower than the amount of the option you\'ve selected.';
+
+// ../templates/default/paypal_form.tpl:4 
+$lang['- ERROR -'] = '- ERROR -';
+
+// ../templates/default/paypal_form.tpl:12 
+$lang['Predefined amounts cannot be loaded, that is not a critical error.'] = 'Predefined amounts cannot be loaded, that is not a critical error.';
+
+// ../templates/default/paypal_form.tpl:87 
+$lang['The amount you\'ve entered is lower than the minimum amount for the selected option.\\nPlease choose another option or change the amount.'] = 'The amount you\'ve entered is lower than the minimum amount for the selected option.\\nPlease choose another option or change the amount.';
+
+// ../templates/default/paypal_form.tpl:5 
+$lang['<strong>Payment coult not work</strong>: An error occured (that has been logged) while loading Paypal preferences from database.<br/>Please report the issue to the staff.'] = '<strong>Payment coult not work</strong>: An error occured (that has been logged) while loading Paypal preferences from database.<br/>Please report the issue to the staff.';
+
+// ../templates/default/paypal_form.tpl:33 
+$lang['Go back to %s Website to complete your inscription.'] = 'Go back to %s Website to complete your inscription.';
+
+// ../templates/default/paypal_form.tpl:6 
+$lang['Our apologies for the annoyance :('] = 'Our apologies for the annoyance :(';
+
+// ../templates/default/paypal_form.tpl:59 
 $lang['Amount'] = 'Amount';
 
-// ../templates/default/paypal_form.tpl:37 
-$lang['Select an option below, then click \'Pay\' to proceed. Once your paiment validated, an entry will be automatically added in the contributions table and staff members will receive a notification mail.'] = 'Select an option below, then click \'Pay\' to proceed. Once your paiment validated, an entry will be automatically added in the contributions table and staff members will receive a notification mail.';
+// ../templates/default/paypal_form.tpl:35 
+$lang['Select an option below, then click \'Payment\' to proceed. Once your paiment validated, an entry will be automatically added in the contributions table and staff members will receive a notification mail.'] = 'Select an option below, then click \'Payment\' to proceed. Once your paiment validated, an entry will be automatically added in the contributions table and staff members will receive a notification mail.';
 
-// ../templates/default/paypal_form.tpl:70 
-$lang['Proceed'] = 'Proceed';
+// ../templates/default/paypal_form.tpl:1 
+$lang['Paypal payment'] = 'Paypal payment';
+
+// ../templates/default/paypal_form.tpl:54 
+$lang['Enter an amount.'] = 'Enter an amount.';
+
+// ../templates/default/paypal_form.tpl:11 
+$lang['- WARNING -'] = '- WARNING -';
+
+// ../templates/default/paypal_form.tpl:81 
+$lang['You have to select an option'] = 'You have to select an option';
+
+// ../templates/default/paypal_form.tpl:63 
+$lang['Payment'] = 'Payment';
+
+// ../templates/default/paypal_form.tpl:51 
+$lang['No predefined amounts have been defined yet.'] = 'No predefined amounts have been defined yet.';
 
 ?>
--- lang/lang_french.php
+++ lang/lang_french.php
@@ -1,15 +1,51 @@
 <?php
-// This file was automatically generated on Fri Jun  3 14:24:25 2011 (GMT)
+// This file was automatically generated on Thu Jun  9 15:21:54 2011 (GMT)
 // Don't modify it by hand, rather use the target lang from the Makefile.
 
 
-// ../templates/default/paypal_form.tpl:66 
+// ../templates/default/paypal_form.tpl:56 
+$lang['WARNING: If you enter an amount below, make sure that it is not lower than the amount of the option you\'ve selected.'] = 'ATTENTION : si vous entrez un montant ci-dessous, assurez-vous qu\'il ne soit pas inférieur au montant de l\'option que vous avez sélectionnée.';
+
+// ../templates/default/paypal_form.tpl:4 
+$lang['- ERROR -'] = '- ERREUR -';
+
+// ../templates/default/paypal_form.tpl:12 
+$lang['Predefined amounts cannot be loaded, that is not a critical error.'] = 'Les montants prédéfinis n\'ont pas pu être chargés, cette erreur n\'est pas crtitique.';
+
+// ../templates/default/paypal_form.tpl:87 
+$lang['The amount you\'ve entered is lower than the minimum amount for the selected option.\\nPlease choose another option or change the amount.'] = 'Le montant que vous avez entreé est inférieur au montant minimum de l\'option sléectionnée.\\nVeuillez choisir une autre option ou entrer un nouveau montant.';
+
+// ../templates/default/paypal_form.tpl:5 
+$lang['<strong>Payment coult not work</strong>: An error occured (that has been logged) while loading Paypal preferences from database.<br/>Please report the issue to the staff.'] = '<strong>Le paiement ne peut fonctionner</strong> : une erreur (qui a été enregistrée) est survenue lors du chargement des préférences Paypal depuis la base de données.<br/>Merci de rapporter ce problème aux membres du bureau.';
+
+// ../templates/default/paypal_form.tpl:33 
+$lang['Go back to %s Website to complete your inscription.'] = 'Retour au site de %s pour compléter votre inscription.';
+
+// ../templates/default/paypal_form.tpl:6 
+$lang['Our apologies for the annoyance :('] = 'Toutes nos excuses pour le désagrément :(';
+
+// ../templates/default/paypal_form.tpl:59 
 $lang['Amount'] = 'Montant';
 
-// ../templates/default/paypal_form.tpl:37 
-$lang['Select an option below, then click \'Pay\' to proceed. Once your paiment validated, an entry will be automatically added in the contributions table and staff members will receive a notification mail.'] = 'Choisissez une option ci-dessous, puis cliquez sur \'Payer\' pour continuer. Une fois que votre paiement aura été validé, une entrée sera automatiquement générée dans la table des contributions et les membres du bureau recevront un message de notification.';
+// ../templates/default/paypal_form.tpl:35 
+$lang['Select an option below, then click \'Payment\' to proceed. Once your paiment validated, an entry will be automatically added in the contributions table and staff members will receive a notification mail.'] = 'Choisissez une option ci-dessous, puis cliquez sur \'Paiement\' pour continuer. Une fois que votre paiement aura été validé, une entrée sera automatiquement générée dans la table des contributions et les membres du bureau recevront un message de notification.';
 
-// ../templates/default/paypal_form.tpl:70 
-$lang['Proceed'] = 'Payer';
+// ../templates/default/paypal_form.tpl:1 
+$lang['Paypal payment'] = 'Paiement Paypal';
+
+// ../templates/default/paypal_form.tpl:54 
+$lang['Enter an amount.'] = 'Entrez un montant.';
+
+// ../templates/default/paypal_form.tpl:11 
+$lang['- WARNING -'] = '- ATTENTION -';
+
+// ../templates/default/paypal_form.tpl:81 
+$lang['You have to select an option'] = 'Vous devez sélectionner une option';
+
+// ../templates/default/paypal_form.tpl:63 
+$lang['Payment'] = 'Paiement';
+
+// ../templates/default/paypal_form.tpl:51 
+$lang['No predefined amounts have been defined yet.'] = 'Aucun montant prédéfini n\'existe.';
 
 ?>
