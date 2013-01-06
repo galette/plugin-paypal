a627dbecb0a14a9d98bdddb9dc292f2406411db6
Johan Cwiklinski <johan@x-tnd.be>
2013-01-06 11:26:04 +0100

    Bump version

--- _define.php
+++ _define.php
@@ -39,7 +39,7 @@ $this->register(
     'Galette Paypal',       //Name
     'Paypal integration',   //Short description
     'Johan Cwiklinski',     //Author
-    '1.1.0.1',                //Version
+    '1.2',                  //Version
     '0.7.3',                //Galette compatible version
     null                    //Permissions needed - not yet implemented
 );
