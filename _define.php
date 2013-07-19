519b0bf43806013bfe5a222ac6d318a96bacae61
Johan Cwiklinski <johan@x-tnd.be>
2013-07-19 20:56:39 +0200

    Update Galette compatiblity version

--- _define.php
+++ _define.php
@@ -40,7 +40,7 @@ $this->register(
     'Paypal integration',   //Short description
     'Johan Cwiklinski',     //Author
     '1.4',                  //Version
-    '0.7.4.1',              //Galette compatible version
+    '0.7.5',                //Galette compatible version
     null                    //Permissions needed - not yet implemented
 );
 
