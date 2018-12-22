88404be0b3fb1c729c10af46c6dbbb7d67b9e79f
Johan Cwiklinski <jcwiklinski@teclib.com>
2018-12-22 21:47:03 +0100

    Update locales

--- lang/paypal_routes_fr_FR.utf8.php
+++ lang/paypal_routes_fr_FR.utf8.php
@@ -1,20 +1,20 @@
 <?php
-// This file was automatically generated on Fri Nov 25 06:11:45 2016 (GMT)
+// This file was automatically generated on Sat Dec 22 20:46:35 2018 (GMT)
 // Don't modify it by hand, rather use the target lang from the Makefile.
 
 
-// ../_routes.php:128 
+// ../_routes.php:116 
 $lang['paypal_routes']['/form'] = '/formulaire';
 
-// ../_routes.php:181 
+// ../_routes.php:469 
+$lang['paypal_routes']['/history/filter'] = '/historique/filtre';
+
+// ../_routes.php:157 
 $lang['paypal_routes']['/success'] = '/succes';
 
-// ../_routes.php:258 
+// ../_routes.php:227 
 $lang['paypal_routes']['/notify'] = '/notify';
 
-// ../_routes.php:438 
-$lang['paypal_routes']['/history'] = '/historique';
-
-// ../_routes.php:168 
+// ../_routes.php:144 
 $lang['paypal_routes']['/cancel'] = '/cancel';
 
