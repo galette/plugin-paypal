0d761dbc6737602d4d59928364e66deca0c6a37f
Johan Cwiklinski <johan@x-tnd.be>
2021-10-23 09:01:14 +0200

    Bump version

--- _define.php
+++ _define.php
@@ -7,7 +7,7 @@
  *
  * PHP version 5
  *
- * Copyright © 2011-2018 The Galette Team
+ * Copyright © 2011-2021 The Galette Team
  *
  * This file is part of Galette (http://galette.tuxfamily.org).
  *
@@ -28,7 +28,7 @@
  * @package   GalettePaypal
  *
  * @author    Johan Cwiklinski <johan@x-tnd.be>
- * @copyright 2011-2018 The Galette Team
+ * @copyright 2011-2021 The Galette Team
  * @license   http://www.gnu.org/licenses/gpl-3.0.html GPL License 3.0 or (at your option) any later version
  * @link      http://galette.tuxfamily.org
  * @since     Available since 0.7dev - 2011-05-31
@@ -38,10 +38,10 @@ $this->register(
     'Galette Paypal',       //Name
     'Paypal integration',   //Short description
     'Johan Cwiklinski',     //Author
-    '1.9.0',                //Version
+    '1.9.1',                //Version
     '0.9.5',                //Galette compatible version
     'paypal',               //routing name and translation domain
-    '2021-07-03',           //Release date
+    '2021-10-23',           //Release date
     [   //Permissions needed
         'paypal_preferences'        => 'staff',
         'store_paypal_preferences'  => 'staff',
