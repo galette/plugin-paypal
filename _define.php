179653f56f45ec22d2b1d1ff791ba8e46a1e6f13
Johan Cwiklinski <johan@x-tnd.be>
2026-02-12 08:31:19 +0100

    Use named parameters

--- _define.php
+++ _define.php
@@ -21,15 +21,16 @@
 
 declare(strict_types=1);
 
+/** @var \Galette\Core\Plugins $this */
 $this->register(
-    'Galette Paypal',       //Name
-    'Paypal integration',   //Short description
-    'Johan Cwiklinski',     //Author
-    '2.2.1',                //Version
-    '1.2.1',                //Galette compatible version
-    'paypal',               //routing name and translation domain
-    '2025-12-08',           //Release date
-    [   //Permissions needed
+    name: 'Galette Paypal',     //Name
+    desc: 'Paypal integration', //Short description
+    author: 'Johan Cwiklinski', //Author
+    version: '2.2.1',           //Version
+    compver: '1.2.1',           //Galette compatible version
+    route: 'paypal',            //routing name and translation domain
+    date: '2025-12-08',         //Release date
+    acls: [                     //Permissions needed
         'paypal_preferences'        => 'staff',
         'store_paypal_preferences'  => 'staff',
         'paypal_history'            => 'staff',
